da1a222ac7992468adb52220e7c186180d8cc609
jirkanovak <jirkanovakneda11@seznam.cz>
2022-01-13 13:38:37 +0100

    starting new project this one will be used as a helper in new one

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :governings
   resources :users
   resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-root "home#index"
+  root "home#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
